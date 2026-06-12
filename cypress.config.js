ae2a67e81bf5a695b11c0552a2e3d9cde22dee3a
unknown <piolozaballero131@gmail.com>
2026-06-13 01:23:28 +0800

    Update cypress config with projectId

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: "3vy8ex",
   e2e: {
     // Since this is a simple static HTML file, we don't need a dev server.
     // We'll visit the file directly using a relative path.
